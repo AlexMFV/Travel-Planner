a20583cdbddaa10fe6c60e299b9b29468a45bf72
Alex Valente <alexmfv1@gmail.com>
2022-05-26 11:49:43 +0100

    Made profile information load with the page

--- public/js/loadCommon.js
+++ public/js/loadCommon.js
@@ -1,24 +1,38 @@
+window.addEventListener('DOMContentLoaded', loadIncludes);
+
+function loadIncludes(){
+  loadName();
+  loadUsername(); 
+}
+
 $(function () {
   //Load all the elements that are common to all pages
-    var includes = $('[data-include]')
-    $.each(includes, function () {
-      var file = $(this).data('include') + '.html'
-      $(this).load(file)
-    })
-
-    //Set the active page
-    //$('#navbar-nav li').removeClass('active');
-    //$('#navbar-nav li').addClass('active');
+  var includes = $('[data-include]')
+  $.each(includes, function () {
+    var file = $(this).data('include') + '.html'
+    $(this).load(file)
   })
 
-  $("tvl_username").load(function(){
-    const elem = document.getElementById("tvl_username");
-    if(elem != null)
-      elem.innerHTML = getUsername();
-  });
+  //Set the active page
+  //$('#navbar-nav li').removeClass('active');
+  //$('#navbar-nav li').addClass('active');
+})
 
-  $("tvl_name").load(function(){
-    const elem = document.getElementById("tvl_name");
-    if(elem != null)
-      elem.innerHTML = getName();
-  });
+function loadName() {
+  const elemName = document.getElementById("tvl_name");
+
+  if (elemName != null)
+    elemName.innerHTML = getName();
+}
+
+function loadUsername(){
+  const elemUsername = document.getElementById("tvl_username");
+
+  if (elemUsername != null)
+    elemUsername.innerHTML = getName();
+}
+
+function loadThings(){
+  loadName();
+  loadUsername();
+}
