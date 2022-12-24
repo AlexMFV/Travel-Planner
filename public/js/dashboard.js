3da59208baf20f8a25f4093c6d96cb95916509ac
AlexMFV <alexmfv1@gmail.com>
2022-12-24 00:50:12 +0000

    Updated calculation methods and

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -7,32 +7,16 @@ async function loadDashboard(){
     glob.flightReportPrevYear = await getMonthlyFlightReportByYear(today.getFullYear()-1);
 
     //Convert the values to flot and calculate total value from this year
-    var totalValue = 0;
-    var totalValuePrevYear = 0;
-    
-    for (var i = 0; i < glob.flightReport.length; i++) {
-        var fvt = parseFloat(glob.flightReport[i].value);
-        var fvtPreviousYear = parseFloat(glob.flightReportPrevYear[i].value);
-
-        totalValue += fvt;
-        totalValuePrevYear += fvtPreviousYear;
-    }
+    var totalValue = calculateFlightValue(glob.flightReport);
+    var totalValuePrevYear = calculateFlightValue(glob.flightReportPrevYear);
     
     //Calculate the change in value from the previous year to this year
-    var flightValueChange = (totalValue - totalValuePrevYear);
-
-    if(totalValuePrevYear == 0)
-        flightValueChange = 100; //If there is no value from the previous year, the change is 100%
-    else
-    {
-        flightValueChange = flightValueChange / totalValue;
-        flightValueChange = flightValueChange * 100;
-    }
+    var flightValueChange = calculateChangePercentage(totalValue, totalValuePrevYear, totalValue);
+    console.log(flightValueChange);
 
     modifyElementText("totalFlightValue", totalValue.toFixed(2) + "€");
     modifyElementText("flightValueChange", percentageElement(flightValueChange.toFixed(2), totalValuePrevYear.toFixed(2)));
     modifyElementClass("flightValueChange", flightValueChange < 0 ? "text-success fw-semibold" : "text-danger fw-semibold");
-    //modifyElementText("totalFlightValuePrevious", totalValuePrevYear.toFixed(2));
 }
 
 async function getMonthlyFlightReportByYear(_year){
@@ -65,4 +49,20 @@ function percentageElement(value, prevValue){
     if(value < 0)
         return '<i class="bx bx-up-arrow-alt"></i>' + value + '% (' + prevValue + '€)';
     return '<i class="bx bx-down-arrow-alt"></i>+' + value + '% (' + prevValue + '€)';
+}
+
+function calculateFlightValue(globVariable){
+    var totalValue = 0;
+    for (var i = 0; i < globVariable.length; i++) {
+        totalValue += parseFloat(globVariable[i].value);
+    }
+    return totalValue;
+}
+
+function calculateChangePercentage(first, last, basedOn){
+    if(basedOn == 0)
+        return 100; //If there is no value from the previous year, the change is 100%
+
+    var change = (first - last) / basedOn;
+    return change * 100;
 }
